57261f2eb97efce4cab53ca012daf3ad7f0f0ee9
asman <asman@o2.pl>
2020-07-15 21:05:48 +0200

    added missing tabs/new line

--- test/assetsTest.c
+++ test/assetsTest.c
@@ -7,61 +7,61 @@
 
 void testAssets(void)
 {
-        ULONG result = MemoryAllocateAll();
+	ULONG result = MemoryAllocateAll();
 
-        if (RT_OK == result)
-        {
-                result = AssetsLoad("exe/data.bin");
+	if (RT_OK == result)
+	{
+		result = AssetsLoad("exe/data.bin");
 
-                if (RT_OK == result)
-                {
-                        ULONG offset = AssetsGet(ASSET_FONTS8);
+		if (RT_OK == result)
+		{
+			ULONG offset = AssetsGet(ASSET_FONTS8);
 
-                        muAssert(offset != 0, "wrong offset address");
-                }
-                else
-                {
-                        muFail("Cannot load assets");
-                }
-        }
-        else
-        {
-                muFail("can't alloc memory");
-        }
+			muAssert(offset != 0, "wrong offset address");
+		}
+		else
+		{
+			muFail("Cannot load assets");
+		}
+	}
+	else
+	{
+		muFail("can't alloc memory");
+	}
 
 
-        MemoryReleaseAll();
+	MemoryReleaseAll();
 }
 
 /*--------------------------------------------------------------------------*/
 
 void testAssetFonts8(void)
 {
-        ULONG result = MemoryAllocateAll();
+	ULONG result = MemoryAllocateAll();
 
-        if (RT_OK == result)
-        {
-                result = AssetsLoad("exe/data.bin");
+	if (RT_OK == result)
+	{
+		result = AssetsLoad("exe/data.bin");
 
-                if (RT_OK == result)
-                {
-                        ULONG* offset = (ULONG*)AssetsGet(ASSET_FONTS8);
+		if (RT_OK == result)
+		{
+			ULONG* offset = (ULONG*)AssetsGet(ASSET_FONTS8);
 
-                        muAssert(*offset == 0xff61e80b, "wrong content");
-                }
-                else
-                {
-                        muFail("Cannot load assets");
-                }
+			muAssert(*offset == 0xff61e80b, "wrong content");
+		}
+		else
+		{
+			muFail("Cannot load assets");
+		}
 
-        }
-        else
-        {
-                muFail("can't alloc memory");
-        }
+	}
+	else
+	{
+		muFail("can't alloc memory");
+	}
 
 
-        MemoryReleaseAll();
+	MemoryReleaseAll();
 }
 
 /*--------------------------------------------------------------------------*/
