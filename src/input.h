45f04a02702ef4242c13f6faa219126bbb03063c
asman <asman@o2.pl>
2020-07-16 08:08:59 +0200

    added input

---
+++ src/input.h
@@ -0,0 +1,11 @@
+#ifndef ALETEREGO_INPUT_H
+#define ALETEREGO_INPUT_H
+/*--------------------------------------------------------------------------*/
+
+#include "types.h"
+
+UBYTE InputMouseLeftButton(void);
+UBYTE InputJoystickRedButton(void);
+
+/*--------------------------------------------------------------------------*/
+#endif /* ALETEREGO_INPUT_H */
