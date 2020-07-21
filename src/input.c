9879036b9db440d3043a4672c757c84e614a6ee0
asman <asman@o2.pl>
2020-07-21 16:29:47 +0200

    added input get joystick state

--- src/input.c
+++ src/input.c
@@ -1,5 +1,9 @@
 #include "input.h"
 
+#include <hardware/custom.h>
+
+extern struct Custom* custom;
+
 /*--------------------------------------------------------------------------*/
 
 #define CIAADDR (volatile UBYTE*)(0xbfe001)
@@ -14,6 +18,24 @@ UBYTE InputMouseLeftButton(void)
 UBYTE InputJoystickRedButton(void)
 {
 	return (!(*CIAADDR & 128));
+}
+
+/*--------------------------------------------------------------------------*/
+
+UBYTE InputJoystickGetState(void)
+{
+	UWORD joy = custom->joy1dat;
+	UBYTE result = 0;
+
+	result += (joy >> 1 ^ joy) & 0x0100 ? JOY_UP : 0;
+	result += (joy >> 1 ^ joy) & 0x0001 ? JOY_DOWN : 0;
+
+	result += joy & 0x0200 ? JOY_LEFT : 0;
+	result += joy & 0x0002 ? JOY_RIGHT : 0;
+
+	result += InputJoystickRedButton() ? JOY_BUTTON_RED : 0;
+
+	return result;
 }
 
 /*--------------------------------------------------------------------------*/
--- src/input.h
+++ src/input.h
@@ -4,8 +4,15 @@
 
 #include "types.h"
 
+#define JOY_BUTTON_RED	0x01
+#define JOY_UP		0x02
+#define JOY_DOWN 	0x04
+#define JOY_LEFT 	0x08
+#define JOY_RIGHT	0x10
+
 UBYTE InputMouseLeftButton(void);
 UBYTE InputJoystickRedButton(void);
 
+UBYTE InputJoystickGetState(void);
 /*--------------------------------------------------------------------------*/
 #endif /* ALTEREGO_INPUT_H */
