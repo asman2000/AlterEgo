6727a300710e9e04a1ab4bee86a1789e35b527cf
asman <asman@o2.pl>
2020-10-04 17:52:45 +0200

    types changed

--- src/types.h
+++ src/types.h
@@ -10,7 +10,6 @@
 #define __reg(string)
 #endif
 
-
 #include <exec/types.h>
 
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
@@ -24,74 +23,6 @@ enum Returns
 	RT_NOT_ENOUGH_ANY_MEM,
 	RT_FAILED_LOAD_DATA,
 };
-
-typedef struct _CopperDetails
-{
-	ULONG address;
-} CopperDetails;
-
-typedef struct _ScreenDetails
-{
-	UWORD bpl;
-	UWORD brow;
-	UWORD height;
-	ULONG address;
-} ScreenDetails;
-
-typedef struct _SpritesDetails
-{
-	ULONG fake;
-	ULONG hero;
-	ULONG ego;
-	ULONG enemies;
-	ULONG data;
-} SpritesDetails;
-
-typedef struct _AssetsDetails
-{
-	ULONG packed;
-	ULONG decrunchStack;
-} AssetsDetails;
-
-typedef struct _GameDetails
-{
-	ULONG level;
-	ULONG tiles;
-
-} GameDetails;
-
-typedef struct _MemoryDetails
-{
-	CopperDetails copper;
-	ScreenDetails screen;
-	SpritesDetails sprites;
-
-	ULONG palette;
-	ULONG creditsText;
-	ULONG smallFont;
-
-	ULONG music;
-	ULONG sfx;
-
-	AssetsDetails assets;
-
-	GameDetails game;
-
-} MemoryDetails;
-
-
-struct MainState;
-typedef void (*StateFunction)(struct MainState*);
-
-struct MainState
-{
-	StateFunction run;
-
-	MemoryDetails* memory;
-
-	UBYTE exitToOs;
-};
-
 
 struct GlobalMainState;
 typedef void (*Fnc)(void);
